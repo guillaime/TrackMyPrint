2d272597c3e8906ede5441bed35e90f3058f55ac
Kevin Vugts <kevin-vugts@live.com>
2016-12-08 15:23:24 +0100

    Added Checkin UI With Working List View - hardcoded

--- app/src/main/java/com/example/tmp/trackmyprint/MainActivity.java
+++ app/src/main/java/com/example/tmp/trackmyprint/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.tmp.trackmyprint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<production_proccess> names = new ArrayList<>();
+        names.add(new production_proccess("Order Received"));
+        names.add(new production_proccess("Printing Pending"));
+        names.add(new production_proccess("Cutting not started"));
+        names.add(new production_proccess("Quality Control not started"));
+        names.add(new production_proccess("Packaging"));
+        names.add(new production_proccess("Shipping"));
+
+        production_proccess_list_adapter a = new production_proccess_list_adapter(this, names);
+        ListView lv = (ListView) findViewById(R.id.production_proccess);
+        lv.setAdapter(a);
+
     }
 }
---
+++ app/src/main/java/com/example/tmp/trackmyprint/production_proccess.java
@@ -0,0 +1,14 @@
+package com.example.tmp.trackmyprint;
+
+/**
+ * Created by fhict on 08/12/2016.
+ */
+
+public class production_proccess {
+
+    public String name;
+
+    public production_proccess(String name) {
+        this.name = name;
+    }
+}
