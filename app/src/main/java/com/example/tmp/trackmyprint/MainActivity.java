ab16b94004d7e5135ee2bdf9763105d416b1e36b
kevin <kevin-vugts@live.com>
2017-01-12 14:15:40 +0100

    Added layout file for the order details per sector .

--- app/src/main/java/com/example/tmp/trackmyprint/MainActivity.java
+++ app/src/main/java/com/example/tmp/trackmyprint/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         checkIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, nfcActivity.class);
+                Intent intent = new Intent(MainActivity.this, printDetails.class);
                 startActivity(intent);
             }
         });
